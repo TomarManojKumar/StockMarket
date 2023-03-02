6423c79944da98a96a1c06061f072546ab38c5f1
Manoj Kumar Tomar <86923599+TomarManojKumar@users.noreply.github.com>
2023-03-02 15:55:11 +0530

    Update practice123.py

--- practice123.py
+++ practice123.py
@@ -1,10 +1,18 @@
 # Importing Liberaries
+
+# Yahoo! Finance API
+# Open-source tool that uses Yahoo's publicly available APIs, and is intended for research and educational purposes
 import yfinance as yf
+
+# Streamlit for web-page
 import streamlit as st
 import pandas as pd
+
+# Base64 for changing format of image
 import base64
 
-def add_bg_from_local(image_file):
+# Creating a function to make a pic as background
+def addImg(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     st.markdown(
@@ -18,14 +26,16 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local('istockphoto-1153657433-612x612.jpg')    
+addImg('istockphoto-1153657433-612x612.jpg')    
 
 # Setting Header and Welcome message
-st.title(":red[Welcome To _Stock_ _Price_ App]\n")
-st.header(":green[See the complete visualization of] :green[_Company_ _Stocks_]")
+st.title(":blue[Welcome To _Stock_ _Price_ App]\n")
+st.header(":green[See the complete visualization of] :green[_Companies_ _Stocks_]")
 
 # Getting user name
 name = st.text_input(":red[Q : What name do you go by ?] :sunglasses: :").capitalize()
+
+# Selecting button 
 button1 = st.button("Click Here")
 
 # Actions to happen after pressing button
@@ -38,13 +48,28 @@ company = st.text_input(":red[Q : Which companies stock do you want to see ?] :"
 startingDate = st.date_input(":red[_Select_ _Starting_ _Date_ (YYYY-MM-DD)] : ")
 endDate = st.date_input(":red[_Select_ _the_ _End_ _Date_ (YYYY-MM-DD)] : ")
 
-# Using yfinance lib to get data
+# Using yfinance lib to get data using API
 tickerSymbol = company.upper()
 tickerData = yf.Ticker(tickerSymbol)
 tickerDF = tickerData.history(period='1d',start= startingDate,end= endDate)
 
-# Plotting graphs for the above information
-st.header(f"\n:blue[Closing Price of {tickerSymbol}]")
-st.line_chart(tickerDF.Close )
-st.header(f"\n:blue[Closing Volume of {tickerSymbol}]")
-st.line_chart(tickerDF.Volume)
+st.header(f"Complete Stock Analysis of {tickerSymbol}")
+st.dataframe(tickerDF,use_container_width=True)
+
+# Defining Columns for better view of 
+col1, col2 = st.columns(2)
+with col1:
+    # Plotting graphs for the above information
+    st.header(f"\n:blue[Closing Price of {tickerSymbol}]")
+    st.line_chart(tickerDF.Close)
+
+    st.header(f"\n:blue[Divident given by {tickerSymbol}]")
+    st.bar_chart(tickerDF.Dividends)
+with col2:
+    st.header(f"\n:blue[Closing Volume of {tickerSymbol}]")
+    st.area_chart(tickerDF.Volume)
+
+    st.header(f"\n:blue[Stock Split of {tickerSymbol}]")
+    st.bar_chart(tickerDF["Stock Splits"])
+
+    
